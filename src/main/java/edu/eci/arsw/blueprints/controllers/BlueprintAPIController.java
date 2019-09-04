cafac94b63e9adc6eac8f6ef020fc9751913faed
Tylones <etiennereitz@gmail.com>
2019-09-04 08:12:24 -0500

    question 2 done

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,9 +5,15 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+@Service
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
     
+    @Autowired
+    BlueprintsServices bs;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getRessourceBlueprints(){
+        try{
+            return new ResponseEntity<>("Worked",HttpStatus.ACCEPTED);
+            
+        }catch(Exception ex){
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE,null,ex);
+            return new ResponseEntity<>("Blueprints not found", HttpStatus.NOT_FOUND);
+        }
+        
+        
+    }
     
     
     
--- src/main/java/edu/eci/arsw/blueprints/main/mainProgram.java
+++ src/main/java/edu/eci/arsw/blueprints/main/mainProgram.java
@@ -13,42 +13,42 @@ import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 public class mainProgram{
 
-    public static void main(String[] args) throws BlueprintNotFoundException, BlueprintPersistenceException, AuthorNotFoundException{
-        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-        BlueprintsServices bps = ac.getBean(BlueprintsServices.class);
-
-        System.out.println("Creating new blueprints...");
-        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15), new Point(40, 40), new Point(35, 35), new Point(35, 35), new Point(35, 35)};
-        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
-
-        Point[] pts1=new Point[]{new Point(20, 20),new Point(10, 10)};
-        Blueprint bp1=new Blueprint("etienne", "arsw",pts1);
-
-        Point[] pts2=new Point[]{new Point(10, 10),new Point(5, 5)};
-        Blueprint bp2=new Blueprint("etienne", "third",pts2);
-
-        System.out.println("Adding new blueprints to the BlueprintService...");
-        bps.addNewBlueprint(bp0);
-        bps.addNewBlueprint(bp1);
-        bps.addNewBlueprint(bp2);
-
-        System.out.println("Retrieving blue print \"mypaint\" from the author \"mack\"");
-        Blueprint b = bps.getBlueprint("mack", "mypaint");
-
-        System.out.println("Name of the retrieved blueprint : " + b.getName());
-        System.out.println("Author of the retrieved blueprint : " + b.getAuthor());
-
-
-        System.out.println("Number of blueprints from the author \"etienne\" : " + bps.getBlueprintsByAuthor("etienne").size());
-
-        System.out.println("Number of points in the filtered blueprint : " + bps.getBlueprint("mack", "mypaint").getPoints().size());
-        
-        System.out.println("Points in the filter blueprint : ");
-        for(Point p : bps.getBlueprint("mack", "mypaint").getPoints()){
-            System.out.println("X : " + p.getX() + "   Y : " + p.getY());
-        }
-        
-
-    }
+//    public static void main(String[] args) throws BlueprintNotFoundException, BlueprintPersistenceException, AuthorNotFoundException{
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        BlueprintsServices bps = ac.getBean(BlueprintsServices.class);
+//
+//        System.out.println("Creating new blueprints...");
+//        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15), new Point(40, 40), new Point(35, 35), new Point(35, 35), new Point(35, 35)};
+//        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+//
+//        Point[] pts1=new Point[]{new Point(20, 20),new Point(10, 10)};
+//        Blueprint bp1=new Blueprint("etienne", "arsw",pts1);
+//
+//        Point[] pts2=new Point[]{new Point(10, 10),new Point(5, 5)};
+//        Blueprint bp2=new Blueprint("etienne", "third",pts2);
+//
+//        System.out.println("Adding new blueprints to the BlueprintService...");
+//        bps.addNewBlueprint(bp0);
+//        bps.addNewBlueprint(bp1);
+//        bps.addNewBlueprint(bp2);
+//
+//        System.out.println("Retrieving blue print \"mypaint\" from the author \"mack\"");
+//        Blueprint b = bps.getBlueprint("mack", "mypaint");
+//
+//        System.out.println("Name of the retrieved blueprint : " + b.getName());
+//        System.out.println("Author of the retrieved blueprint : " + b.getAuthor());
+//
+//
+//        System.out.println("Number of blueprints from the author \"etienne\" : " + bps.getBlueprintsByAuthor("etienne").size());
+//
+//        System.out.println("Number of points in the filtered blueprint : " + bps.getBlueprint("mack", "mypaint").getPoints().size());
+//        
+//        System.out.println("Points in the filter blueprint : ");
+//        for(Point p : bps.getBlueprint("mack", "mypaint").getPoints()){
+//            System.out.println("X : " + p.getX() + "   Y : " + p.getY());
+//        }
+//        
+//
+//    }
 
 }
