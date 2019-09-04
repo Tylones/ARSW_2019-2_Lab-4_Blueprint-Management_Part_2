c91ab0b17e64539c840137b397d9561b7ced8c64
Tylones <etiennereitz@gmail.com>
2019-09-04 07:57:10 -0500

    modified bpp constructor

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -31,7 +31,20 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
         //load stub data
         Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
         Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15), new Point(40, 40), new Point(35, 35), new Point(35, 35), new Point(35, 35)};
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+
+        Point[] pts1=new Point[]{new Point(20, 20),new Point(10, 10)};
+        Blueprint bp1=new Blueprint("etienne", "arsw",pts1);
+
+        Point[] pts2=new Point[]{new Point(10, 10),new Point(5, 5)};
+        Blueprint bp2=new Blueprint("etienne", "third",pts2);
+        
         blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        blueprints.put(new Tuple<>(bp0.getAuthor(),bp0.getName()), bp0);
+        blueprints.put(new Tuple<>(bp1.getAuthor(),bp1.getName()), bp1);
+        blueprints.put(new Tuple<>(bp2.getAuthor(),bp2.getName()), bp2);
         
     }    
     
